a44431b28c66ce8c4c6fff1fa830b88ff81ea8ba
Robin Dong <robin.k.dong@gmail.com>
2023-12-19 18:30:41 +1100

    Let training loop show both train and test accuracy

--- tests/cifar100_train.py
+++ tests/cifar100_train.py
@@ -22,31 +22,42 @@ def loss_fn(params, images, targets):
 
 
 with open("cifar-100-python/train", "rb") as fp:
-    batch1 = pickle.load(fp, encoding="bytes")
+    train_data = pickle.load(fp, encoding="bytes")
+
+with open("cifar-100-python/test", "rb") as fp:
+    test_data = pickle.load(fp, encoding="bytes")
 
 # Training loop
+CLASSES = 100
 num_epochs = 1000
 batch_size = 1024
 learning_rate = 1e-1
-dataset = device_put(jnp.array(batch1[b"data"], dtype="float32"))
-dlabels = device_put(jnp.array(batch1[b"fine_labels"]))
-length = len(dataset) // batch_size
+train_ds = device_put(jnp.array(train_data[b"data"], dtype="float32"))
+train_labels = device_put(jnp.array(train_data[b"fine_labels"]))
+test_ds = device_put(jnp.array(test_data[b"data"], dtype="float32"))
+test_images = jnp.reshape(test_ds, (len(test_ds), 3, 32, 32))
+test_labels = nn.one_hot(device_put(jnp.array(test_data[b"fine_labels"])), CLASSES)
 
 loss_and_grad_fn = jit(jax.value_and_grad(loss_fn))
 update_fn = jit(functools.partial(jax.tree_map, lambda p, g: p - learning_rate * g))
 
-params = ResNet.init([2, 2, 2, 2], classes=100, bottleneck=False, scale=1e-2)  # resnet18
+params = ResNet.init(
+    [2, 2, 2, 2], classes=CLASSES, bottleneck=False, scale=1e-2
+)  # resnet18
 for name in params.keys():
     print(name, params[name].shape)
 
+train_len = len(train_ds) // batch_size
+
 for epoch in range(num_epochs):
     train_acc = 0.0
-    for start in range(length):
+    for start in range(train_len):
         begin, end = start * batch_size, (start + 1) * batch_size
-        images = dataset[begin:end]
+        images = train_ds[begin:end]
         images = jnp.reshape(images, (batch_size, 3, 32, 32))
-        labels = nn.one_hot(dlabels[begin:end], 100)
+        labels = nn.one_hot(train_labels[begin:end], CLASSES)
         loss, grads = loss_and_grad_fn(params, images, labels)
         params = update_fn(params, grads)
         train_acc += accuracy(params, images, labels)
-    print(f"[{epoch}] Train accu:", train_acc / length)
+    test_acc = accuracy(params, test_images, test_labels)
+    print(f"[{epoch}] Train accu:", train_acc / train_len, ". Test accu:", test_acc)
